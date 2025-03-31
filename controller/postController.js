cc069766c1caa694e7ec678a8db3ba2813426f59
Deni0012 <deni.dg00@icloud.com>
2025-03-31 13:11:07 +0200

    feat: function index with filter tag, function show with status 404

--- controller/postController.js
+++ controller/postController.js
@@ -2,30 +2,60 @@ const posts = require("../data/posts.js");
 
 //index
 function index(req, res){
+    let filterPosts = posts;
+    
+    if (req.query.tags) {
+        const tagsArray = Array.isArray(req.query.tags) ? req.query.tags : [req.query.tags];
+        console.log(tagsArray);
+        filterPosts = posts.filter(post => {
+            return tagsArray.every(queryTags => {
+
+                const indexQueryTag = queryTags.toLowerCase().replace(/\s/g, '+');
+
+                return post.tags.some(postTags => {
+                    const indexPostTag = postTags.toLowerCase().replace(/\s/g, '+');
+
+                    return indexPostTag.includes(indexQueryTag);
+                });
+            });
+        });
+    }
+    res.json(filterPosts)
 }
 
 //show
 function show(req, res){
+   const id = parseInt(req.params.id);
+   const post = posts.find(post => post.id === id);
 
+   if(post){
+    res.json(post);
+   } else {
+    res.status(404).json({
+        messaggio: "Error page not found"
+    });
+   }
 }
 
 //store
 function store(req, res){
-
+    res.send("store function");
 }
 
 //update
 function update(req, res){
+    res.send("update function");
 
 }
 
 //patch
 function patch(req, res){
-
+    res.send("patch function")
 }
 
 //destroy
 function destroy(req, res){
+    res.send("destroy function");
 
 }
 
