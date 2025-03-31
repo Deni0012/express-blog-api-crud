d7f71b832d903c257733d27f9e4c3afdc8d762cd
Deni0012 <deni.dg00@icloud.com>
2025-03-31 12:05:31 +0200

    feat: import posts.js and router post.js in app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-const expresse = require("express");
+const express = require("express");
+const postRouter = require("./router/post.js");
+const posts = require("./data/posts.js");
+const app = express();
+const port = 5500;
+
+app.use(express.static('public'));
+
+app.use("/posts", postRouter);
+
+app.listen(port, function(){
+    console.log("Server attivo");
+});
