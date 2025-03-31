c704ce99c0f1bd59d44e6202037aa4f220ed3d86
Deni0012 <deni.dg00@icloud.com>
2025-03-31 16:44:04 +0200

    feat: fix app.js

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const controller = require("./controller/postController.js");
 const app = express();
 const port = 5500;
 
-app.use(express.static('../public'));
+app.use(express.static('/public'));
 
-app.use(express.static ('../posts.js' + posts));
+app.use(express.static ('/posts.js' + posts));
 
-app.use(express.static('../postController.js' + controller));
+app.use(express.static('/postController.js' + controller));
 
-app.use('../post.js', postRouter);
+app.use('/post.js', postRouter);
 
 app.listen(port, function(){
     console.log("Server attivo");
