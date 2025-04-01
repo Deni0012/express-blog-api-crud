e9682afdc513ff1d0b296c2aa1acb83932a3dde1
Deni0012 <deni.dg00@icloud.com>
2025-04-01 13:04:10 +0200

    add: notFound e errorsHandler creati ed importati in app.js

--- app.js
+++ app.js
@@ -2,19 +2,33 @@ const express = require("express");
 const postRouter = require("./router/post.js");
 const posts = require("./data/posts.js");
 const controller = require("./controller/postController.js");
+const notFound = require("./middlewares/notFound.js");
+const errorsHandler = require("./middlewares/error.js");
 const app = express();
 const port = 5500;
 
+//rotta public
 app.use(express.static('/public'));
 
+//rotta json
 app.use(express.json());
 
+//rotta posts
 app.use(express.static ('/posts.js' + posts));
 
+//rotta Controller
 app.use(express.static('/postController.js' + controller));
 
+//rotta postRouter
 app.use('/post.js', postRouter);
 
+//rotta notFound
+app.use(notFound);
+
+//rotta errorsHandler
+app.use(errorsHandler);
+
+//rotta server
 app.listen(port, function(){
     console.log("Server attivo");
 });
